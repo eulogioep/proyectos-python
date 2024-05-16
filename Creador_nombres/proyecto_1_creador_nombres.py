ceb5295b1e5843f409bcacc81df60bd712de71ce
eulogioep <eulogio-ep@hotmail.com>
2024-05-16 14:30:12 +0200

    Proyecto 1

--- Creador_nombres/proyecto_1_creador_nombres.py
+++ Creador_nombres/proyecto_1_creador_nombres.py
@@ -2,3 +2,14 @@
 # requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
 # combinadas, para formar una marca creativa.
 
+ciudad = input("¿Qué ciudad te gusta mas? ")
+color = input("¿Cuá es tu color favorito? ")
+
+
+print("El nombre de tu marca es:\n", ciudad, color) 
+
+# También se puede hacer todo en una sola línea pero los datos se pierden al no
+# guardar dichos datos en variablese.
+
+# print("El nombre de tu marca es\n" + input("¿Que ciudad te gusta más?: ") +
+#        " " + input("¿Cual es tu color favorito?: "))
