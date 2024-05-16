6dd96e1bfb63eb0dd7973a13c954ee838be1d867
eulogioep <eulogio-ep@hotmail.com>
2024-05-16 14:05:32 +0200

    Inicio Repositorio

---
+++ Creador_nombres/proyecto_1_creador_nombres.py
@@ -0,0 +1,4 @@
+# Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que
+# requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
+# combinadas, para formar una marca creativa.
+
